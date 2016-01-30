ca751c90fcbc5e905cce83045d6a709ee5a36168
TheJP <jp_55555@hotmail.com>
2016-01-30 03:44:47 +0100

    Implemented enemy spawning

--- Assets/GAME/Scripts/UnitSpawner.cs
+++ Assets/GAME/Scripts/UnitSpawner.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 class UnitSpawner : MonoBehaviour, IUnitGenerator
 {
-    public Transform UnitsParent;
+    public Transform unitsParent;
     public GameObject shadowStalker;
+
+    public int minimumShadowStalker = 1;
+    public int maximumShadowStalker = 5;
 
     /// <summary>
     /// Returns units that are already children of UnitsParent object
@@ -15,12 +17,12 @@ class UnitSpawner : MonoBehaviour, IUnitGenerator
     public List<Unit> SpawnUnits(List<Cell> cells)
     {
         List<Unit> result = new List<Unit>();
-        for (int i = 0; i < UnitsParent.childCount; i++)
+        for (int i = 0; i < unitsParent.childCount; i++)
         {
-            var unit = UnitsParent.GetChild(i).GetComponent<Unit>();
+            var unit = unitsParent.GetChild(i).GetComponent<Unit>();
             if (unit != null)
             {
-                var cell = cells.OrderBy(h => Math.Abs((h.transform.position - unit.transform.position).magnitude)).First();
+                var cell = cells.OrderBy(h => System.Math.Abs((h.transform.position - unit.transform.position).magnitude)).First();
                 if (!cell.IsTaken)
                 {
                     cell.IsTaken = true;
@@ -38,13 +40,41 @@ class UnitSpawner : MonoBehaviour, IUnitGenerator
             {
                 Debug.LogError("Invalid object in Units Parent game object");
             }
-
         }
+        SpawnEnemies(cells, result);
         return result;
     }
 
+    /// <summary>Spawns all needed enemies.</summary>
     private void SpawnEnemies(List<Cell> cells, List<Unit> result)
     {
-        
+        SpawnEnemiesOfType(cells, result, shadowStalker, minimumShadowStalker, maximumShadowStalker);
+    }
+
+    /// <summary>
+    /// Spawns enemies of the given prefab type.
+    /// </summary>
+    /// <param name="cells"></param>
+    /// <param name="result">New enemies will be added to this list.</param>
+    /// <param name="prefab">Prefab, which determines the enemy type.</param>
+    /// <param name="min">Minimum number of enemies to be spawned.</param>
+    /// <param name="max">Maximum number of enemies to be spawned.</param>
+    private void SpawnEnemiesOfType(List<Cell> cells, List<Unit> result, GameObject prefab, int min, int max)
+    {
+        var enemies = Random.Range(min, max);
+        for (int i = 0; i < enemies; ++i)
+        {
+            //Choose cell where the shadow stalker should be spawned
+            Cell cell;
+            do { cell = cells.GetRandomElement(); }
+            while (cell.IsTaken);
+            var enemyObject = (GameObject)Instantiate(prefab, cell.transform.position, Quaternion.identity);
+            enemyObject.transform.parent = unitsParent.transform;
+            //Initialize unit
+            var unit = enemyObject.GetComponent<Unit>();
+            unit.Cell = cell;
+            unit.Initialize();
+            result.Add(unit);
+        }
     }
 }
