b5637c676c8a87680edf551ae1e440a9740faebb
TheJP <jp_55555@hotmail.com>
2016-01-30 01:34:55 +0100

    Finished basic setup of the room scene

---
+++ Assets/MainController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour
+{
+    public CellGrid cellGrid;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            cellGrid.EndTurn();
+        }
+    }
+}
